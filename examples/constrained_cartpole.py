1e2520e2044eeeb1973cea83b5581f2d809dda41
Iacob Casian <casiacob@gmail.com>
2024-07-25 16:53:43 +0300

    cartpole experiment

--- examples/constrained_cartpole.py
+++ examples/constrained_cartpole.py
@@ -4,14 +4,16 @@ from jax import config
 import matplotlib.pyplot as plt
 from admm_noc.utils import discretize_dynamics
 from jax import lax, debug
-from admm_noc.utils import wrap_angle, rollout
+from admm_noc.utils import wrap_angle, rollout, euler
 from admm_noc.par_admm_optimal_control import par_admm
-
+from admm_noc.ddp_admm_optimal_control import ddp_admm
+import time
+import pandas as pd
 
 # Enable 64 bit floating point precision
 config.update("jax_enable_x64", True)
 
-config.update("jax_platform_name", "cpu")
+config.update("jax_platform_name", "cuda")
 
 def projection(z):
     x_min = jnp.finfo(jnp.float64).min
@@ -83,37 +85,75 @@ def cartpole(
     )
 
 
-simulation_step = 0.05
-downsampling = 1
-dynamics = discretize_dynamics(
-    ode=cartpole, simulation_step=simulation_step, downsampling=downsampling
-)
+penalty_parameter = 0.5
+Ts = [0.05, 0.025, 0.0125, 0.01, 0.005, 0.0025, 0.00125, 0.001]
+N = [20, 40, 80, 100, 200, 400, 800, 1000]
+ddp_time_means = []
+ddp_time_medians = []
+par_time_means = []
+par_time_medians = []
 
-horizon = 15
-key = jax.random.PRNGKey(271)
-u_init = jnp.array([0.01]) * jax.random.normal(key, shape=(horizon, 1))
-x0_init = jnp.array([0.01, wrap_angle(-0.01), 0.01, -0.01])
-x_init = rollout(dynamics, u_init, x0_init)
-z_init = jnp.zeros((horizon, u_init.shape[1] + x_init.shape[1]))
-l_init = jnp.zeros((horizon, u_init.shape[1] + x_init.shape[1]))
-sigma = 0.1
 
-def mpc_loop(carry, input):
-    jax.debug.print('------------------------')
-    x0, u, z, l = carry
+for sampling_period, horizon in zip(Ts, N):
+    ddp_time_array = []
+    par_time_array = []
+    downsampling = 1
+    dynamics = euler(cartpole, sampling_period)
+
+    x0 = jnp.array([wrap_angle(0.1), -0.1])
+    key = jax.random.PRNGKey(1)
+    u = 0.1 * jax.random.normal(key, shape=(horizon, 1))
     x = rollout(dynamics, u, x0)
-    x, u, z, l, iterations = par_admm(
-        transient_cost, final_cost, dynamics, projection, x, u, z, l, sigma
-    )
-    return (x[1], u, z, l), (x[1], u[0], iterations)
+    z = jnp.zeros((horizon, u.shape[1] + x.shape[1]))
+    l = jnp.zeros((horizon, u.shape[1] + x.shape[1]))
 
-jitted_mpc_loop = jax.jit(mpc_loop)
-_, (mpc_x, mpc_u, n_iterations) = jax.lax.scan(
-    jitted_mpc_loop, (x0_init, u_init, z_init, l_init), xs=None, length=100
-)
-plt.plot(mpc_x[:, 0])
-plt.plot(mpc_x[:, 1])
-plt.show()
-plt.plot(mpc_u[:, 0])
-plt.show()
-print(jnp.sum(n_iterations))
+    annon_par_admm = lambda x_, u_, z_, l_ : par_admm(
+        transient_cost, final_cost, dynamics, projection, x_, u_, z_, l_, penalty_parameter
+    )
+    annon_ddp = lambda x_, u_, z_, l_ : ddp_admm(
+        transient_cost, final_cost, dynamics, projection, x_, u_, z_, l_, penalty_parameter
+    )
+    _jitted_par = jax.jit(annon_par_admm)
+    _jitted_ddp = jax.jit(annon_ddp)
+
+    _, _, _, _, _ = _jitted_par(x, u, z, l)
+    _, _, _, _, _ = _jitted_ddp(x, u, z, l)
+    for i in range(10):
+        print(i)
+
+        start = time.time()
+        _, u_par_admm, _, _, _ = _jitted_par(x, u, z, l)
+        jax.block_until_ready(u_par_admm)
+        end = time.time()
+        par_time = end - start
+        print("par finished")
+
+        start = time.time()
+        _, u_ddp_admm, _, _, _ = _jitted_ddp(x, u, z, l)
+        jax.block_until_ready(u_ddp_admm)
+        end = time.time()
+        ddp_time = end - start
+        print("seq finished")
+
+        ddp_time_array.append(ddp_time)
+        par_time_array.append(par_time)
+
+    ddp_time_means.append(jnp.mean(jnp.array(ddp_time_array)))
+    ddp_time_medians.append(jnp.median(jnp.array(ddp_time_array)))
+    par_time_means.append(jnp.mean(jnp.array(par_time_array)))
+    par_time_medians.append(jnp.median(jnp.array(par_time_array)))
+
+seq_time_means_arr = jnp.array(ddp_time_means)
+seq_time_medians_arr = jnp.array(ddp_time_medians)
+par_time_means_arr = jnp.array(par_time_means)
+par_time_medians_arr = jnp.array(par_time_medians)
+
+df_means_ddp = pd.DataFrame(seq_time_means_arr)
+df_median_ddp = pd.DataFrame(seq_time_medians_arr)
+df_mean_par = pd.DataFrame(par_time_means_arr)
+df_median_par = pd.DataFrame(par_time_medians_arr)
+
+df_means_ddp.to_csv("cartpole_admm_means_ddp.csv")
+df_median_ddp.to_csv("cartpole_admm_medians_ddp.csv")
+df_mean_par.to_csv("cartpole_admm_means_par.csv")
+df_median_par.to_csv("cartpole_admm_medians_par.csv")
